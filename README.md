4b1b04ec4d9b8812c22f4449d4bf96f4fae246d9
Lander-Hatsune <wangwwx@yeah.net>
2022-03-17 17:10:29 +0800

    siri-like effect

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# music2vid
-Music collection as self-made videos
+# music2vid #
+
+![./screenshots/test1.png](demo)
+
+原来 b 站给没版权的音乐做个动效就能投自制, 贫穷而机智如我.
+
+- 动效尝试复现 Siri, Matplotlib 硬画
+- 视频编辑 MoviePy
+
+Music collection as self-made videos, with a Siri-like music visualizer
+
+- Tried to reproduce Siri-like spectrum effect, with Matplotlib
+- Video editing with MoviePy
+
+# Ref list #
+
+- [https://stackoverflow.com/a/26712790](Additive blending with Matplotlib)
+- [https://www.geeksforgeeks.org/moviepy-creating-animation-using-matplotlib/](Animation from Matplotlib to MoviePy)
+- [https://matplotlib.org/stable/gallery/images_contours_and_fields/barcode_demo.html#sphx-glr-gallery-images-contours-and-fields-barcode-demo-py](Matplotlib barcode ex)
+- [https://www.avnishparker.com/AfterEffectsTemplates/SIRI-AUDIO-SPECTRUM](Similiar effect with AdobeAE)
+
+
+
+
+
--- visualizer.py
+++ visualizer.py
@@ -3,49 +3,62 @@ from moviepy.video.io.bindings import mplfig_to_npimage
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import numpy as np
+from scipy import interpolate
 
+BAND_W = 100
+INTERP_L = 100
 
 class Visualizer:
 
     def __init__(self, audio):
-        self.audio = audio.to_soundarray()[:, 0]
+        self.audio = audio.to_soundarray()
+        self.audio = (self.audio[:, 0] + self.audio[:, 1]) # 2 channels
         self.fps = audio.fps
-        self.window = np.hamming(self.fps // 24)
-        self.x = np.linspace(0, 10, 50)
-
-        plt.box(False)
+        self.window = np.hanning(BAND_W)
+        self.x = np.linspace(0, 10, BAND_W)
+        self.x_s = np.linspace(0, 10, INTERP_L)
+        self.prev_y_s = np.zeros(INTERP_L)
 
         self.fig = plt.figure()
         self.ax = self.fig.add_axes([0, 0, 1, 1])
 
         DPI = self.fig.get_dpi()
-        self.fig.set_size_inches(1000.0 / float(DPI), 200.0 / float(DPI))
+        self.fig.set_size_inches(1920.0 / float(DPI), 200.0 / float(DPI))
+
+    def _draw(self, spec, c):
+
+        spl = interpolate.splrep(self.x, spec * self.window, s=1)
+        y_s = np.tanh(interpolate.splev(self.x_s, spl))
+
+        self.ax.clear()
+        self.ax.set_axis_off()
+        self.ax.set_ylim(bottom=-1, top=1)
+        self.ax.set_xlim(left=0, right=10)
+        self.ax.add_patch(Rectangle((0, -1), 10, 2, color='k'))
+        self.ax.fill_between(self.x_s, y_s, color=c)
+        self.ax.fill_between(self.x_s, -y_s, color=c)
+        ret = mplfig_to_npimage(self.fig).copy()
+        return ret
 
     def visualize(self, f):
 
-        if (f + 1 / 24) * self.fps > len(self.audio):
-            return mplfig_to_npimage(self.fig)
+        if (f + 1) * self.fps > len(self.audio) or (f - 1) < 0:
+            return self._draw(np.zeros(BAND_W), c='k')
 
-        frame = self.audio[int(f * self.fps):int((f + 1 / 24) * self.fps)]
+        frame = self.audio[int((f - 1) * self.fps):int((f + 1) * self.fps)]
         spec = np.abs(np.fft.fftshift(frame))
 
         checkpoint = len(spec) // 8
 
-        r_spec = spec[checkpoint:checkpoint + 50]
-        g_spec = spec[checkpoint * 2:checkpoint * 2 + 50]
-        b_spec = spec[checkpoint * 3:checkpoint * 3 + 50]
+        r_spec = spec[checkpoint:checkpoint + BAND_W]
+        g_spec = spec[checkpoint * 2:checkpoint * 2 + BAND_W]
+        b_spec = spec[checkpoint * 3:checkpoint * 3 + BAND_W]
 
+        fig_r = self._draw(r_spec, c='#F00')
+        fig_g = self._draw(g_spec, c='#0F0')
+        fig_b = self._draw(b_spec, c='#00F')
 
-        self.ax.clear()
-        self.ax.set_axis_off()
-        self.ax.set_ylim(bottom=0, top=1)
-        self.ax.set_xlim(left=0, right=10)
-        self.ax.add_patch(Rectangle((0, 0), 10, 1, color='k'))
-        self.ax.fill_between(self.x, r_spec, color='#1a8bd9', alpha=0.2)
-        self.ax.fill_between(self.x, g_spec, color='#cf277f', alpha=0.2)
-        self.ax.fill_between(self.x, b_spec, color='#25c9c0', alpha=0.2)
-
-        return mplfig_to_npimage(self.fig)
+        return np.maximum(np.maximum(fig_r, fig_g), fig_b)
         
 
         
