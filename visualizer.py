e54051925c5ea627b8200f83120c16733aa66984
Lander-Hatsune <wangwwx@yeah.net>
2022-04-14 20:09:55 +0800

    debug, modify freq bands, x/ylim, afterfetch, no splrep

--- visualizer.py
+++ visualizer.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import numpy as np
 from scipy.signal import savgol_filter
-from scipy import interpolate
+from scipy.interpolate import splrep, splev
 
-BAND_W = 512
-INTERP_L = 100
+BAND_R = np.arange(16, 160) # 40 ~ 400
+BAND_G = np.arange(160, 600) # 150 ~ 1500
+BAND_B = np.arange(600, 1600) # 400 ~ 4000
 
 class Visualizer:
 
@@ -15,55 +16,67 @@ class Visualizer:
         self.audio = audio.to_soundarray()
         self.audio = np.hypot(self.audio[:, 0], self.audio[:, 1])
         self.fps = audio.fps
-        half_hann = np.hanning(2 * INTERP_L // 3)[0:INTERP_L // 3]
-        self.window = np.concatenate((half_hann,
-                                      np.ones(INTERP_L - 2 * len(half_hann)),
-                                      half_hann[::-1]))
-        self.x = np.linspace(0, 10, BAND_W)
-        self.x_s = np.linspace(0, 10, INTERP_L)
-
         self.fig = plt.figure()
         self.ax = self.fig.add_axes([0, 0, 1, 1])
 
         DPI = self.fig.get_dpi()
         self.fig.set_size_inches(1920.0 / float(DPI), 200.0 / float(DPI))
 
-    def _draw(self, spec, c):
-
-        y = savgol_filter(spec, BAND_W // 2, 3)
-        spl = interpolate.splrep(self.x, y)
-        y_s = interpolate.splev(self.x_s, spl) * self.window
-        
+    def _prepare(self):
         self.ax.clear()
         self.ax.set_axis_off()
-        self.ax.set_ylim(bottom=-1.5, top=1.5)
-        self.ax.set_xlim(left=0, right=10)
-        self.ax.add_patch(Rectangle((0, -1.5), 10, 3, color='k'))
-        self.ax.fill_between(self.x_s, y_s, color=c)
-        self.ax.fill_between(self.x_s, -y_s, color=c)
+        self.ax.set_ylim(bottom=-1.2, top=1.2)
+        self.ax.set_xlim(left=0, right=1)
+        self.ax.add_patch(Rectangle((0, -1.2), 1, 1.2 * 2, color='k'))
 
+    def _draw(self, spec, c):
+        self._prepare()
+
+        spec = spec[::len(spec) // 128]
+        spec = spec / (np.max(spec) + 0.00001)
+
+        y = savgol_filter(spec, len(spec) // 3, 5)
+        y *= np.hanning(len(y))
+        x = np.linspace(0, 1, len(y))
+        self.ax.fill_between(x, y, color=c)
+        self.ax.fill_between(x, -y, color=c)
+
+        ret = mplfig_to_npimage(self.fig).copy()
+        return ret
+
+    def _draw_debug(self, spec):
+
+        DPI = self.fig.get_dpi()
+        self.fig.set_size_inches(1920.0 / float(DPI), 1080.0 / float(DPI))
+
+        self.ax.clear()
+        self.ax.grid()
+        self.ax.plot(spec)
+        self.ax.axvline(BAND_R[0], color="r")
+        self.ax.axvline(BAND_R[-1], color="r")
+        self.ax.axvline(BAND_G[0], color="g")
+        self.ax.axvline(BAND_G[-1], color="g")
+        self.ax.axvline(BAND_B[0], color="b")
+        self.ax.axvline(BAND_B[-1], color="b")
+        self.ax.set_ylim([0, 1000])
         ret = mplfig_to_npimage(self.fig).copy()
         return ret
 
     def visualize(self, f):
 
-        if (f + 0.1) * self.fps > len(self.audio) or (f - 0.1) < 0:
-            return self._draw(np.zeros(BAND_W), c='k')
+        if (f + 0.1) * self.fps > len(self.audio) or (f - 0.3) < 0:
+            return self._draw(np.zeros(len(BAND_R)), c='k')
 
-        frame = self.audio[int((f - 0.1) * self.fps):int((f + 0.1) * self.fps)]
-        spec = np.abs(np.fft.fftshift(frame))
+        frame = self.audio[int((f - 0.3) * self.fps):int((f + 0.1) * self.fps)]
+        spec = np.abs(np.fft.rfft(frame))
 
-        checkpoint = len(spec) // 6
-
-        r_spec = spec[1:BAND_W + 1]
-        g_spec = spec[checkpoint * 1:checkpoint * 1 + BAND_W]
-        b_spec = spec[checkpoint * 2:checkpoint * 2 + BAND_W]
-
-        fig_r = self._draw(r_spec, c='#F00')
-        fig_g = self._draw(g_spec, c='#0F0')
-        fig_b = self._draw(b_spec, c='#00F')
+        fig_r = self._draw(spec[BAND_R], c='#F00')
+        fig_g = self._draw(spec[BAND_G], c='#0F0')
+        fig_b = self._draw(spec[BAND_B], c='#00F')
 
         return np.maximum(np.maximum(fig_r, fig_g), fig_b)
+
+    # return self._draw_debug(spec)
         
 
         
