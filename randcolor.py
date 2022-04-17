cfd7a62254005ddedcfdd6ccbc7839a7020625fd
Lander-Hatsune <wangwwx@yeah.net>
2022-04-17 23:49:34 +0800

    use hsv for randcolor

--- randcolor.py
+++ randcolor.py
@@ -1,13 +1,14 @@
+from matplotlib.colors import hsv_to_rgb
 import numpy as np
 
 def get3Colors(seed:int=814):
-
     np.random.seed(seed)
-    randc = lambda: np.random.random() * 0.5
-    r = (1, randc(), randc())
-    g = (randc(), 1, randc())
-    b = (randc(), randc(), 1)
-    return (r, g, b)
+    randhue = np.random.random()
+    decimal = lambda x: x - int(x)
+    c1 = hsv_to_rgb((randhue, 1, 1))
+    c2 = hsv_to_rgb((decimal(randhue + 1/3), 1, 1))
+    c3 = hsv_to_rgb((decimal(randhue + 2/3), 1, 1))
+    return (c1, c2, c3)
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
