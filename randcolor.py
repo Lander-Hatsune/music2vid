7cbb4d5d31477980d1578464528a35fe4c2e8173
Lander-Hatsune <wangwwx@yeah.net>
2022-04-18 21:57:45 +0800

    fix rand color

--- randcolor.py
+++ randcolor.py
@@ -1,13 +1,12 @@
 from matplotlib.colors import hsv_to_rgb
 import numpy as np
 
-def get3Colors(seed:int=814):
-    np.random.seed(seed)
-    randhue = np.random.random()
+def get3Colors(seed:int):
+    randhue = (seed % 814) / 814 / 3
     decimal = lambda x: x - int(x)
     c1 = hsv_to_rgb((randhue, 1, 1))
-    c2 = hsv_to_rgb((decimal(randhue + 1/3), 1, 1))
-    c3 = hsv_to_rgb((decimal(randhue + 2/3), 1, 1))
+    c2 = hsv_to_rgb((randhue + 1/3, 1, 1))
+    c3 = hsv_to_rgb((randhue + 2/3, 1, 1))
     return (c1, c2, c3)
 
 if __name__ == "__main__":
